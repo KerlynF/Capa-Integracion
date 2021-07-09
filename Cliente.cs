41780c1902e81818de00550bbb33ba7857f341d4
KerlynF <78883400+KerlynF@users.noreply.github.com>
2021-07-09 01:25:39 -0400

    Add files via upload

---
+++ Cliente.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace IntegrationServer
+{
+    class Cliente
+    {
+        public static int Main(string[] args)
+        {
+            StartClient();
+            return 0;
+        }
+
+
+        public static void StartClient()
+        {
+            byte[] bytes = new byte[1024];
+
+            try
+            {
+                // Se conecta a un server remoto  
+                // obtiene la direccion ip del host para establecer la conexion
+                // este ejemplo es de localhost 
+                
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                IPAddress ipAddress = host.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+                // Crea un TCP/IP socket.    
+                Socket sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                
+
+                // Usando try catch para conectarse al end point y capturar cualquier error.    
+                try
+                {
+                    // conectándose al end point remoto  
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket conectado a la IP: {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    // codifica la data para enviar    
+                    byte[] msg = Encoding.ASCII.GetBytes("Probando...\0");
+
+                    // envía la data     
+                    int bytesSent = sender.Send(msg);
+
+                    // Recibe la respuesta del servidor    
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("Echoed test = {0}",
+                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    // suelta el socket.    
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ Servidor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace IntegrationConsole
+{
+    class Servidor
+    {
+        public static int Main(string[] args)
+        {
+            StartServer();
+
+            return 0;
+        }
+
+
+        public static void StartServer()
+        {
+            // obtiene la direccion ip del host para establecer la conexion
+            // este ejemplo es de localhost  
+
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+
+            try
+            {
+
+                // Crea un Socket usando el protocolo Tcp       
+                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                // Asocio el socket con el end point de destino usando el metodo Bing 
+                listener.Bind(localEndPoint);
+               
+               
+                // La cantidad de request que acepta el socket a la vez antes de devolver que está ocupado 
+                // (Puede estar sujeto a cambios) 
+                listener.Listen(100);
+
+                Console.WriteLine("Esperando una conexión...");
+                Socket handler = listener.Accept();
+
+                // Obteniendo la data del cliente    
+                string data = null;
+                byte[] bytes = null;
+
+                while (true)
+                {
+                    bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+                    
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    if (data.IndexOf("\0") > -1)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Información recibida : {0}", data);
+
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+                handler.Send(msg);
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\n Presione cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
+
